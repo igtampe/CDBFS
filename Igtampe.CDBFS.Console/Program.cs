eaeeb30c90a09c0fcb33f658293873cb11b6a3eb
Ignacio Tampe <Igtampe@gmail.com>
2022-09-01 23:37:32 -0400

    Make tiny test program

--- Igtampe.CDBFS.Console/Program.cs
+++ Igtampe.CDBFS.Console/Program.cs
@@ -6,10 +6,14 @@ namespace Igtampe.CDBFS.CLI {
     public class Program {
         private async static Task Main(string[] args) {
 
-            
+            if (File.Exists("A:/cdbfs1.sqlite")) { File.Delete("A:/cdbfs1.sqlite"); }
+            ICdbfsDAO D = await CdbfsSqliteDAO.CreateCdbfsSqliteFile("A:/cdbfs1.sqlite");
+            await D.CreateFile("Dingus.txt", File.ReadAllBytes("A:/Dingus.txt"));
+            await D.CreateFile("ChopoNoPauses.png", File.ReadAllBytes("A:/ChopoNoPauses.png"));
 
-
+            foreach (CdbfsFile F in await D.GetFiles()) {
+                Console.WriteLine($"{F.Name} : {F.DateCreated} : {F.DateUpdated}");
+            }
         }
     }
-
 }
