c1438b41d0d293a1b177deb9d26cc53eb12590fe
Ignacio Tampe <Igtampe@gmail.com>
2022-09-02 00:04:17 -0400

    Use ItemName as this will be used for Folders and Files

--- Igtampe.CDBFS.Common/CdbfsPath.cs
+++ Igtampe.CDBFS.Common/CdbfsPath.cs
@@ -1,28 +1,28 @@
 ﻿namespace Igtampe.CDBFS.Common {
     public class CdbfsPath {
 
-        public string FolderPath { get; set; } = "";
+        public string ItemPath { get; set; } = "";
 
-        public string Filename { get; set; } = "";
+        public string ItemName { get; set; } = "";
 
         public static CdbfsPath ParsePath(string Path) {
             //find the index of the last /
             int LastSlash = Path.LastIndexOf('/');
             if (LastSlash == -1) {
                 //This must be in the root directory. Though this is a badly formed path, we will still love it
-                return new() { FolderPath="/", Filename=Path};
+                return new() { ItemPath="/", ItemName=Path};
             }
 
             return new() {
-                FolderPath = Path[..LastSlash], //Anything before the index of is the folder path
-                Filename = Path[(LastSlash+1)..] //Anything after it is the filename
+                ItemPath = Path[..LastSlash], //Anything before the index of is the folder path
+                ItemName = Path[(LastSlash+1)..] //Anything after it is the filename
             };
         }
 
-        public override string ToString() => $"{FolderPath}/{Filename}";
+        public override string ToString() => $"{ItemPath}/{ItemName}";
 
         public override bool Equals(object? obj) => obj is not null && obj is CdbfsPath P && P.ToString()==ToString();
 
-        public override int GetHashCode() => HashCode.Combine(FolderPath, Filename);
+        public override int GetHashCode() => HashCode.Combine(ItemPath, ItemName);
     }
 }
