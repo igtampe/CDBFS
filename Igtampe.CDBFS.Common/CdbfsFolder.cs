f9a5e717bbfd00d534df36e1b44d02e510d38e4d
Ignacio Tampe <Igtampe@gmail.com>
2022-09-01 23:37:25 -0400

    Create CDBFS Folder

---
+++ Igtampe.CDBFS.Common/CdbfsFolder.cs
@@ -0,0 +1,11 @@
+﻿namespace Igtampe.CDBFS.Common {
+    public class CdbfsFolder : Nameable{
+        public string Name { get; set; } = "";
+
+        public string Path { get; set; } = "";
+
+        public List<string> Subfolders { get; set; } = new();
+
+        public List<string> Files { get; set; } = new();
+    }
+}
