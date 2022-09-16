4eb53484102b42177086e4340ee0ed0cb877a5f9
Ignacio Tampe <Igtampe@gmail.com>
2022-09-16 15:18:40 -0400

    #1 Reorg for folders

--- Igtampe.CDBFS.Common/CdbfsFile.cs
+++ Igtampe.CDBFS.Common/CdbfsFile.cs
@@ -2,30 +2,20 @@
 using System.Text.Json.Serialization;
 
 namespace Igtampe.CDBFS.Common {
-
-    /// <summary>A CDBFS File</summary>
-    public class CdbfsFile : AutomaticallyGeneratableIdentifiable {
-
-        /// <summary>Name of this file</summary>
-        public string Name { get; set; } = "";
-
-        /// <summary>Date this file was created</summary>
-        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
-
-        /// <summary>Date this file was updated</summary>
-        public DateTime DateUpdated { get; set; } = DateTime.UtcNow;
-
+    public class CdbfsFile : CdbfsItem {
+        
         /// <summary>Whether or not this file is loaded </summary>
         public bool IsLoaded => DataHolder != null;
 
         /// <summary>Data of this file</summary>
         [JsonIgnore]
+        [NotMapped]
         public byte[] Data {
-            get => DataHolder?.Data ?? Array.Empty<byte>(); 
+            get => DataHolder?.Data ?? Array.Empty<byte>();
             set {
                 if (DataHolder != null) { DataHolder.Data = value; } 
                 else DataHolder = new() { Data = value };
-            } 
+            }
         }
 
         /// <summary>Data holder of this file</summary>
---
+++ Igtampe.CDBFS.Common/CdbfsItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Igtampe.CDBFS.Common {
+
+    /// <summary>A CDBFS File</summary>
+    public abstract class CdbfsItem : AutomaticallyGeneratableIdentifiable {
+
+        /// <summary>Name of this item</summary>
+        public string Name { get; set; } = "";
+
+        /// <summary>Path to the folder where this item is located in</summary>
+        public string FolderPath { get; set; } = "/";
+
+        /// <summary>Date this file was created</summary>
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        /// <summary>Date this file was updated</summary>
+        public DateTime DateUpdated { get; set; } = DateTime.UtcNow;
+        
+    }
+}
