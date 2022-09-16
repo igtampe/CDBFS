923e16129ecf4530b4ee0b33edc85dd2de1d85bb
Ignacio Tampe <Igtampe@gmail.com>
2022-09-16 14:10:07 -0400

    Refactor this to be used in EF

--- Igtampe.CDBFS.Common/CdbfsFile.cs
+++ Igtampe.CDBFS.Common/CdbfsFile.cs
@@ -1,11 +1,34 @@
-﻿namespace Igtampe.CDBFS.Common {
-    public class CdbfsFile {
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Igtampe.CDBFS.Common {
+
+    /// <summary>A CDBFS File</summary>
+    public class CdbfsFile : AutomaticallyGeneratableIdentifiable {
+
+        /// <summary>Name of this file</summary>
         public string Name { get; set; } = "";
 
+        /// <summary>Date this file was created</summary>
         public DateTime DateCreated { get; set; } = DateTime.UtcNow;
 
+        /// <summary>Date this file was updated</summary>
         public DateTime DateUpdated { get; set; } = DateTime.UtcNow;
+
+        /// <summary>Whether or not this file is loaded </summary>
+        public bool IsLoaded => DataHolder != null;
+
+        /// <summary>Data of this file</summary>
+        public byte[] Data {
+            get => DataHolder?.Data ?? Array.Empty<byte>(); 
+            set {
+                if (DataHolder != null) { DataHolder.Data = value; } 
+                else DataHolder = new() { Data = value };
+            } 
+        }
+
+        /// <summary>Data holder of this file</summary>
+        [ForeignKey("CdbfsFileData")]
+        public CdbfsFileData? DataHolder { get; set; } = null;
 
     }
 }
---
+++ Igtampe.CDBFS.Common/CdbfsFileData.cs
@@ -0,0 +1,11 @@
+﻿namespace Igtampe.CDBFS.Common {
+
+    /// <summary>A CDBFS File</summary>
+    public class CdbfsFileData : AutomaticallyGeneratableIdentifiable{
+        
+        public CdbfsFile? Metadata { get; set; } = null;
+
+        public byte[] Data { get; set; } = Array.Empty<byte>();
+
+    }
+}
